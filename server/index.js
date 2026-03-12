436264f38242992f83b92b804abd44595dcf7e89
khushabkhan223 <khushabkhan223@gmail.com>
2026-03-13 00:49:48 +0530

    enable CORS for production frontend

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express()
 const PORT = process.env.PORT || 3001
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  origin: ['http://localhost:5173', 'https://triggerly-green.vercel.app'],
   credentials: true,
 }))
 app.use(express.json())
